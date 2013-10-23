bdfd020c1432ea87fe8a22356ec37aa383dfdaa1
Simon Thelwall <simon.thelwall@gmail.com>
2013-10-23 11:48:32 +0100

    Added headline figures to master.R

--- master.R
+++ master.R
@@ -1,11 +1,16 @@
+library(car) # for deltaMethod
 library(plyr)
 library(binom)
+library(knitr)
 library(xtable)
+library(lmtest) # for coeftest, for robust std errors
+library(survey) # for svycontrast, for interactions. 
 library(ggplot2)
 library(reshape2)
+library(sandwich) # for sandwich function, for robust std errors
 library(lubridate) # for quarter function. Q1 is jan-mar
 
-memory.limit(size = 3000)
+memory.limit(size = 3500)
 options(scipen = 8)
 
 percent <- function(n, d){
@@ -24,7 +29,7 @@ simpleCap<-function(x){
 
 setwd("F:\\antimicrobial resistance\\Simon\\thorax2\\FIS\\poster\\R code")
 
-df <- read.csv("F:\\antimicrobial resistance\\Simon\\thorax2\\SuperlrtallorgFINAL_fixed4.txt", 
+df <- read.csv("F:\\antimicrobial resistance\\Simon\\thorax2\\SuperlrtallorgFINAL_fixed4_w_region.txt", 
                header = TRUE, sep = "\t", stringsAsFactors = FALSE)
 
 names(df) <- tolower(names(df))
@@ -37,7 +42,7 @@ df$year <- as.character(format(df$earliest.specimen.date, "%Y"))
 df$quarter <- quarter(df$earliest.specimen.date)
 df$yrqtr <- paste(df$year, df$quarter, sep = "-")
 df$age.grouping <- NULL
-df$lab.name <- NULL
+#df$lab.name <- NULL
 df$specimen.accession.number <- NULL
 df$patient.hospital.number <- NULL
 df$soundex <- NULL
@@ -45,14 +50,35 @@ df$earliest.specimen.week <- NULL
 df$earliest.specimen.quarter..based.on.date <- NULL
 df$specimen.sequence.number <- NULL
 df$earliest.specimen.month.and.year <- NULL
+gc()
 
+# Headline figures ####
+# number of labs
+length(levels(factor(df$lab.name)))
+table(df$year, length(levels(factor(df$lab.name))) )
+n.labs <- ddply(df, .(year, quarter), summarise, n = length(levels(factor(lab.name))) )
+n.labs 
+n.labs$yr.qtr <- paste(n.labs$year, n.labs$quarter, sep = "-")
+qplot(x = yr.qtr, y = n, data = n.labs, geom = "point")
+
+# specimen type
+table(df$specimen.type.description, df$uniq)
+sum(df$uniq)
+sum(df$uniq[df$specimen.type.description == "SPUTUM"])
+round( (sum(df$uniq[df$specimen.type.description == "SPUTUM"])/sum(df$uniq))*100 , 2)
+
+# specimen source
+table(df$specimen.source.type.description, df$uniq)
+sum(df$uniq[df$specimen.source.type.description == "GENERAL PRACTITIONER"])
+round( (sum(df$uniq[df$specimen.source.type.description == "GENERAL PRACTITIONER"])/sum(df$uniq))*100 , 2)
+
+# figures and tables ####
 source("fig1.R", echo = FALSE)
 
 # Classify resistances
 source("F:\\antimicrobial resistance\\Simon\\thorax2\\non_susceptibility.R")
 
 # Resistance to any recommended ceph ####
-
 abx <- subset(df, antimicrobial.name=="CEFTRIAXONE" | antimicrobial.name=="CEFOTAXIME" | 
                 antimicrobial.name=="CEFUROXIME", select = c(opie.id, antimicrobial.name))
 #head(abx)
@@ -83,3 +109,7 @@ gc()
 
 # fig 2 ####
 source("fig2.R", echo = FALSE)
+
+# table 1 #### incident rate ratios
+source("table1.R", echo = FALSE)
+
